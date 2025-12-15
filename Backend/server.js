1b88476b29a02ac95f6f3a5c2c05b859891e84ba
sunidhi1598 <sunidhi15k@gmail.com>
2025-12-15 13:07:16 +0530

    Day 2: Implement JWT authentication and protected routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
+import authRoutes from "./routes/authRoutes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 
@@ -9,6 +12,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+
 
 // DB connection
 connectDB();
